a6607365c9ce72a01e4210ec4d698d08dcee1801
Holly Phillips <hollymphillips.dev@gmail.com>
2026-06-03 10:21:46 -0700

    started splitting old and new files

---
+++ src/helpers/auth.js
@@ -0,0 +1,13 @@
+/* 
+realistically, the first thing I should do is check cache/storage
+for accesstoken.
+--> if I have one, I can try it out. 
+    // jk, no-- I will store expiry date. check that.
+--> --> I'm in? sweet.
+--> --> no good? let's check out our options.
+
+if I have a refresh token that isn't expired, I should use it.
+
+
+
+*/
