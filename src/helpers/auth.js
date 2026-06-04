94a4f72863b3bfb5267363b166c4254897367438
Holly Phillips <hollymphillips.dev@gmail.com>
2026-06-04 15:00:49 -0700

    more notes

--- src/helpers/auth.js
+++ src/helpers/auth.js
@@ -8,6 +8,30 @@ for accesstoken.
 
 if I have a refresh token that isn't expired, I should use it.
 
+....
 
+access tokens die after 4 hours, so when we store them, 
+let's store them with an expiry date. then we can check
+that, rather than bothering to try an AT that might be
+very expired.
+
+...where is it that I think I'm gonna safely store any 
+cache on an electron app?
+if I store it on my server that could be safe in theory,
+but what would be the point-- it would take a fetch
+to get it. I might as well just fetch w/ my refresh token.
+wait... lol where am I storing the refresh token?
+
+okay maybe both are stored on the server, I fetch them,
+and then I look at what I have, and make a decision 
+based on that data, where I should fetch next:
+- refresh the access token (RT valid, AT expired)
+- start the oauth flow (RT and AT both expired)
+
+okay, so RTs expire after 30 days. I likely won't hit 
+this bc when I get a new AT, I'll get a new RT,
+but I should store an RT expiry regardless to
+save me a fetch.
 
 */
+
--- src/main.js
+++ src/main.js
@@ -3,3 +3,20 @@ const fs = require('fs').promises;
 const path = require('path');
 
 const tmi = require('tmi.js');
+const dbd = require('./tools/voting/voting');
+// importing some reconnect stuff.
+
+const config = require('./helpers/config.json');
+
+const { store, template, base64icon,
+    checkForFile, checkAllKTFs, fallbackKTF
+} = require('./helpers/helpers');
+
+const TWITCH_CHANNEL = 'videovomit';
+
+let mainWindow, client;
+/*
+    declared in previous version:
+    clientId, oauthCode, refreshToken, accessToken, username
+*/
+const twitchChannel = `#${TWITCH_CHANNEL}`;
